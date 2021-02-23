046514f30a436f63778f1a128a33ecba033f39e3
ericfish12 <ericfish1221@gmail.com>
2021-02-23 14:45:12 -0500

    prettier, responsive, mvc

--- Exam/index.js
+++ Exam/index.js
@@ -1,27 +1,32 @@
-let array = [];
+const Model = (() => {
+  let searchResult = [];
+  return {
+    searchResult,
+  };
+})();
 
-const render = (element, htmlTemplate) => {
-  element.innerHTML = htmlTemplate;
-};
-const initTmp = (arr) => {
-  let template = "";
-  arr.forEach((ele) => {
-    template += `
-                <div class = 'card'>
-                <img src = '${ele.artworkUrl60}'alt="Avatar" key='${ele.artistId}'  />
-                <h4><b>${ele.artistName}</b></h4> 
-                </div>
-            `;
-  });
-  return template;
-};
+const View = (() => {
+  const render = (element, htmlTemplate) => {
+    element.innerHTML = htmlTemplate;
+  };
+  const initTmp = (arr) => {
+    let template = "";
+    arr.forEach((ele) => {
+      template += `
+                      <div class = 'card'>
+                      <img src = '${ele.artworkUrl60}'alt="Avatar" key='${ele.artistId}'  />
+                      <h4><b>${ele.artistName}</b></h4> 
+                      </div>
+                  `;
+    });
+    return template;
+  };
 
-
-const container = document.getElementsByClassName("content")[0];
-const searchBar = document.getElementById("search_bar");
-
-
-
+  return {
+    render,
+    initTmp,
+  };
+})();
 
 const todoAPI = (() => {
   const searchAllAlbums = (ARTIST_NAME) =>
@@ -33,22 +38,35 @@ const todoAPI = (() => {
   };
 })();
 
+const AppController = (() => {
+  const container = document.getElementsByClassName("content")[0];
+  const searchBar = document.getElementById("search_bar");
 
+  const addListenerOnSearch = () => {
+    searchBar.addEventListener("keyup", (event) => {
+      if (event.key === "Enter") {
+        let currentSearch = event.target.value;
 
+        todoAPI.searchAllAlbums(currentSearch).then((data) => {
+          Model.searchResult = data.results;
+          let count = data.resultCount;
+          document.getElementsByTagName(
+            "h6"
+          )[0].innerText = `${count} Albums for ${currentSearch}`;
 
-
-
-searchBar.addEventListener("keyup", (event) => {
-  if (event.key === "Enter") {
-    let currentSearch = event.target.value;
-
-    todoAPI.searchAllAlbums(currentSearch).then((data) => {
-      array = data.results;
-      let count = data.resultCount;
-      // console.log(data)
-      document.getElementsByTagName("h6")[0].innerText = `${count} Albums for ${currentSearch}`;
-
-      render(container, initTmp(array));
+          View.render(container, View.initTmp(Model.searchResult));
+        });
+      }
     });
-  }
-});
+  };
+
+  const init = async () => {
+    addListenerOnSearch();
+  };
+
+  return {
+    init,
+  };
+})();
+
+AppController.init();
