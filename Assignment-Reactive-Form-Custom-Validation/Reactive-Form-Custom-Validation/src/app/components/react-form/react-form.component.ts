9b9bd8b8694d1376d295257d05d596f48475903c
ericfish12 <ericfish1221@gmail.com>
2021-03-06 22:30:22 -0500

    fixed confirm password first bug by setErrors(null) when value equal

--- Assignment-Reactive-Form-Custom-Validation/Reactive-Form-Custom-Validation/src/app/components/react-form/react-form.component.ts
+++ Assignment-Reactive-Form-Custom-Validation/Reactive-Form-Custom-Validation/src/app/components/react-form/react-form.component.ts
@@ -1,49 +1,52 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
-import{FormGroup,FormBuilder,Validators,FormControl,FormsModule} from'@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+  FormsModule,
+} from '@angular/forms';
 import { equal } from 'src/app/customsValidator/confirmPassword.validator';
 import { User } from 'src/app/Model/User';
 @Component({
   selector: 'app-react-form',
   templateUrl: './react-form.component.html',
-  styleUrls: ['./react-form.component.scss']
+  styleUrls: ['./react-form.component.scss'],
 })
-
-
-
 export class ReactFormComponent implements OnInit {
-users:User[]=[];
-rForm:FormGroup
+  users: User[] = [];
+  rForm: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
-
-    this.rForm= new FormGroup({
-name:new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z][a-zA-Z ]+')]),
-contact: new FormControl('', [Validators.required, Validators.pattern('[0-9]*'),
-Validators.minLength(10),
-Validators.maxLength(10)]),
-email: new FormControl('', [Validators.required, Validators.email]),
-password: new FormControl('', [Validators.required,Validators.minLength(8),]),
-confirm: new FormControl('', [equal]),
-})
-    
-
-
-
-
-
+    this.rForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[a-zA-Z][a-zA-Z ]+'),
+      ]),
+      contact: new FormControl('', [
+        Validators.required,
+        Validators.pattern('[0-9]*'),
+        Validators.minLength(10),
+        Validators.maxLength(10),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      confirm: new FormControl('', [equal]),
+    });
   }
 
-onChange(){
-  console.log(this.rForm.get('confirm'))
-equal(this.rForm.get('confirm'))
+  onChange() {
+    console.log(this.rForm.get('confirm'));
+    equal(this.rForm.get('confirm'));
+  }
 
+  onSubmit() {
+    console.log(this.rForm.value);
+    this.users.push(this.rForm.value);
+  }
 }
-
-onSubmit(){
-  console.log(this.rForm.value)
-  this.users.push(this.rForm.value)
-}
-}
-// export var array:string[]
--- Assignment-Reactive-Form-Custom-Validation/Reactive-Form-Custom-Validation/src/app/customsValidator/confirmPassword.validator.ts
+++ Assignment-Reactive-Form-Custom-Validation/Reactive-Form-Custom-Validation/src/app/customsValidator/confirmPassword.validator.ts
@@ -1,32 +1,13 @@
-import { AbstractControl, ValidationErrors } from "@angular/forms";
-// import { array } from "../components/react-form/react-form.component";
+import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export function equal(control: AbstractControl): ValidationErrors | null {
+  const conVal = control.value;
+  const paVal = control.parent && control.parent.controls['password'].value;
 
-     const v = control.value;
-console.log(222222)
-
-    // const password: string = control.get('password').value; // get password from our password form control
-    // const confirmPassword: string = control.get('confirmPassword').value; // get password from our confirmPassword form control
-    // // compare is the password math
-    // if (password !== confirmPassword) {
-    //     return { 'equal': true, 'requiredSame': 'same' };
-
-    // }
-
-const r = control.parent&&control.parent.controls['password'].value
-console.log(v,r)
-    if (v!=r) {
-        control.setErrors({'equal':true})
-        return { 'equal': true, 'requiredSame': 'same' };
-    }
-
-    // if (v <= 10) {
-
-    //     return { 'equal': true, 'requiredSame': 'same' }
-
-    // }
-
-    return null;
-
+  if (conVal != paVal) {
+    control.setErrors({ equal: true });
+    return { equal: true, requiredSame: 'same' };
+  }
+  control.setErrors(null);
+  return null;
 }
