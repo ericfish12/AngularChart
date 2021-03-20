8da337e42946ccbbf246cda4e70944e429b97415
ericfish12 <ericfish1221@gmail.com>
2021-03-20 00:47:49 -0400

    querySelectorAll<HTMLElement>(.block)

--- Angular-Practice-reuseComponent/src/app/components/block/block.component.ts
+++ Angular-Practice-reuseComponent/src/app/components/block/block.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,Input} from '@angular/core';
+import { Component, OnInit ,Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-block',
@@ -8,7 +8,9 @@ import { Component, OnInit ,Input} from '@angular/core';
 export class BlockComponent implements OnInit {
 
   @Input() color!:string
-
+selected!:string
+  @Output() boxStyle
+// boxStyle!: string;
   constructor() { }
 
   ngOnInit(): void {
@@ -17,20 +19,28 @@ console.log(this.color)
   }
 onclick(co:string){
 
-let obj = {
-
-  red:0,
-  black:1,
-  green:2,
-  blue:3
-}
+this.selected = this.color;
 
 
+document.getElementsByTagName('h1')[0].style.color = this.color;
+
+// this
+
+// document.getElementById("red").style.boxShadow='unset';
+// document.getElementById("black").style.boxShadow='unset';
+// document.getElementById("blue").style.boxShadow='unset';
+// document.getElementById("green").style.boxShadow='unset';
+
+document.querySelectorAll<HTMLElement>('.block').forEach((e)=>{e.style.boxShadow='unset';})
+let what = document.getElementsByClassName('block')[0]
+console.log(what)
 
 
-document.getElementsByTagName('h1')[0].style.color = this.color
 
-document.getElementsByTagName('div')[obj[co]+1].style.boxShadow=`10px 5px 5px ${co}`;
+
+
+
+document.getElementById(this.color).style.boxShadow=`10px 5px 5px ${co}`;
 
 // nodes[0].style.boxShadow="10px 5px 5px red";
 
